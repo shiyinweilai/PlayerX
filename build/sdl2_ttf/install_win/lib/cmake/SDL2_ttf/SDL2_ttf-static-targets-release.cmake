59a644d4be819542e1794304b844ad054b9043e5
shiyinweilai <739277193@qq.com>
2026-02-04 11:50:50 +0800

    【变更】重构文件结构

---
+++ build/sdl2_ttf/install_win/lib/cmake/SDL2_ttf/SDL2_ttf-static-targets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "SDL2_ttf::SDL2_ttf-static" for configuration "Release"
+set_property(TARGET SDL2_ttf::SDL2_ttf-static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(SDL2_ttf::SDL2_ttf-static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libSDL2_ttf.a"
+  )
+
+list(APPEND _cmake_import_check_targets SDL2_ttf::SDL2_ttf-static )
+list(APPEND _cmake_import_check_files_for_SDL2_ttf::SDL2_ttf-static "${_IMPORT_PREFIX}/lib/libSDL2_ttf.a" )
+
+# Import target "SDL2_ttf::freetype" for configuration "Release"
+set_property(TARGET SDL2_ttf::freetype APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(SDL2_ttf::freetype PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;RC"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfreetype.a"
+  )
+
+list(APPEND _cmake_import_check_targets SDL2_ttf::freetype )
+list(APPEND _cmake_import_check_files_for_SDL2_ttf::freetype "${_IMPORT_PREFIX}/lib/libfreetype.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
